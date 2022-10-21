67a88fa1f911d98b1a89e305c1a6366b15386050
Ryan Rhodes <ryan@ryanrhodes.io>
2022-10-21 04:16:10 -0800

    feat(fixes): :bug: fixed minor bugs (#11)

--- src/components/FullScreenHero.tsx
+++ src/components/FullScreenHero.tsx
@@ -7,7 +7,7 @@ interface IFullScreenHeroProps {
 
 const FullScreenHero: FC<IFullScreenHeroProps> = ({ image, altText }) => {
 	return (
-		<div className="mb-16 relative w-full h-96 lg:h-fs">
+		<div className="mb-16 relative w-full sm:h-96 lg:h-fs">
 			<img
 				src={image}
 				alt={altText}
