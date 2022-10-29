4883562c6af3fea74f896cf8f27a87854839c22b
Ryan Rhodes <ryan@ryanrhodes.io>
2022-10-28 23:05:24 -0400

    Revert "feat(partytown): integration setup (#16)"
    This reverts commit 385907be2ad825d650ff0c28204f282dbc585c43.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import path from "path";
-import vercel from "@astrojs/vercel/serverless";
 
-import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })],
-  output: "server",
-  adapter: vercel()
+	integrations: [tailwind(), react()],
+	output: "server",
+	adapter: vercel(),
 });
