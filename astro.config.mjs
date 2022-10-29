2b58495d06af2a07903a6e5136b8aea56552c0ca
Ryan Rhodes <ryan@ryanrhodes.io>
2022-10-28 23:12:26 -0400

    fix(partytown): removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import path from "path";
 
 import vercel from "@astrojs/vercel/serverless";
 
