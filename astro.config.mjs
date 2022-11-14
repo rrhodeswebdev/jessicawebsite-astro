9e6253986f65504bc2920a33b16c7ab6b14e0b46
Ryan Rhodes <ryan@ryanrhodes.io>
2022-11-13 21:19:34 -0700

    feat: added partytown configuration and ga setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: "server",
-	adapter: vercel(),
+  integrations: [tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
+  output: "server",
+  adapter: vercel()
 });
