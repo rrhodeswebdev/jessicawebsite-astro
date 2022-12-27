b1c354def97d82d1abc01436a3ae53a6f9ba6b95
Ryan Rhodes <ryan@ryanrhodes.io>
2022-12-27 11:14:47 -0500

    fix(urls): broken links

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
+export const config = {
+    matcher: '/'
+}
+
 export default function middelware(request: any) {
     const url = new URL(request.url);
 
     if (url.pathname === '/about/camilla') {
-        url.pathname === '/about/';
+        url.pathname === '/about';
     }
 
     return Response.redirect(url);
