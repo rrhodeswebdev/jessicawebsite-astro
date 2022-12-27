59db1b9ce5ba2483e98ac0887a494f102a34f9bf
Ryan Rhodes <ryan@ryanrhodes.io>
2022-12-27 11:56:19 -0500

    fix(redirect): updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
-import { rewrite } from '@vercel/edge';
+// import { rewrite } from '@vercel/edge';
+//
+// export default function middelware(request: Request) {
+//     const url = new URL(request.url);
+//
+//     if (url.pathname.startsWith('/about/camilla')) {
+//         return rewrite(new URL('/about', request.url));
+//     }
+// }
 
-export default function middelware(request: Request) {
-    const url = new URL(request.url);
+export const config = {
+    matcher: '/about/camilla'
+}
 
-    if (url.pathname.startsWith('/about/camilla')) {
-        return rewrite(new URL('/about', request.url));
-    }
+export default function middleware(request: Request) {
+    return Response.redirect(new URL('/about', request.url))
 }
