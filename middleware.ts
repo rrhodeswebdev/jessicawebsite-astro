6fdf8d9794e42e500fee32eed3df171865238527
Ryan Rhodes <ryan@ryanrhodes.io>
2022-12-27 11:48:23 -0500

    fix(urls): updated middleware for redirects

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { rewrite } from '@vercel/edge';
 export default function middelware(request: Request) {
     const url = new URL(request.url);
 
-    if (url.pathname === '/about/camilla') {
+    if (url.pathname.startsWith('/about/camilla')) {
         return rewrite(new URL('/about', request.url));
     }
 }
