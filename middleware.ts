65651efcf311b298fd4481fc9574ccd0ad3716eb
Ryan Rhodes <ryan@ryanrhodes.io>
2022-12-27 11:58:13 -0500

    chore(redirects): updated middleware and removed old package

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,3 @@
-// import { rewrite } from '@vercel/edge';
-//
-// export default function middelware(request: Request) {
-//     const url = new URL(request.url);
-//
-//     if (url.pathname.startsWith('/about/camilla')) {
-//         return rewrite(new URL('/about', request.url));
-//     }
-// }
-
 export const config = {
     matcher: '/about/camilla'
 }
